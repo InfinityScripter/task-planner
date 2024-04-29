a402f0562cbe8f8d323cad01b557419fafe62887
InfinityScripter <talalaev.misha@gmail.com>
2024-04-29 12:01:08 +0300

    day component : add simple toDo save in localStorage service

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import moment from "moment";
+import {LocalStorageService} from "../local-storage.service";
+
+interface Task {
+  id: number;
+  content: string;
+}
 
 @Component({
   selector: 'app-day',
@@ -10,11 +16,28 @@ import moment from "moment";
 export class DayComponent implements OnInit {
   date: string | null = null;
   formattedDate: string | null = null;
+  tasks: Task[] = [];
+  nextTaskId = 0;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private localStorageService: LocalStorageService) {}
 
   ngOnInit() {
     this.date = this.route.snapshot.paramMap.get('date');
     this.formattedDate = this.date ? moment(this.date, 'YYYY-MM-DD').format('DD.MM.YYYY') : null;
+    this.tasks = this.localStorageService.getItem(this.date || 'default') || [];
+    this.nextTaskId = this.tasks.length > 0 ? Math.max(...this.tasks.map(task => task.id)) + 1 : 0;
+  }
+
+  updateTasks(newTasks: Task[]) {
+    this.tasks = newTasks;
+    this.localStorageService.setItem(this.date || 'default', this.tasks);
+  }
+
+  addTask(content: string) {
+    this.updateTasks([...this.tasks, {id: this.nextTaskId++, content}]);
+  }
+
+  removeTask(id: number) {
+    this.updateTasks(this.tasks.filter(task => task.id !== id));
   }
 }
