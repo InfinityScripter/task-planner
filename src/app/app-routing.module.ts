92c4cd3def10af9dfec1f0a4573812b090c51a8f
InfinityScripter <talalaev.misha@gmail.com>
2024-04-29 11:06:26 +0300

    routing : fix main page calendar visibility

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DayComponent} from "./day/day.component";
+import {CalendarComponent} from "./calendar/calendar.component";
 
 const routes: Routes = [
+  { path: '', component: CalendarComponent },
   { path: 'day/:date', component: DayComponent },
 
 ];
