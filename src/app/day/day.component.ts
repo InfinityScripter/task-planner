2481062c4b31754f2ad3c0e1176a4dc082f85f06
InfinityScripter <talalaev.misha@gmail.com>
2024-04-29 11:09:57 +0300

    day component : format date | need refactor

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import moment from "moment";
 
 @Component({
   selector: 'app-day',
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.css']
 })
-export class DayComponent {
-  date: Date;
+export class DayComponent implements OnInit {
+  date: string | null = null;
+  formattedDate: string | null = null;
 
-  constructor(private route: ActivatedRoute) {
-    const dateParam = this.route.snapshot.paramMap.get('date');
-    this.date = dateParam ? new Date(dateParam) : new Date();
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.date = this.route.snapshot.paramMap.get('date');
+    this.formattedDate = this.date ? moment(this.date, 'YYYY-MM-DD').format('DD.MM.YYYY') : null;
   }
 }
